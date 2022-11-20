11174c17c87e0db53ca85f79d890f6f2e112232a
oscarhumo <116611070+oscarhumo@users.noreply.github.com>
2022-11-19 19:48:51 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,73 @@
+basic.forever(function () {
+    music.startMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.Forever)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . # # # #
+        . # . . .
+        . # # # #
+        . . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # . . . .
+        # . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        # # # # .
+        # . . # .
+        # # # # .
+        # . # . .
+        # . . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # # # # #
+        # . . . #
+        # . . . #
+        `)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # # # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        # . # . #
+        # . . . #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+})
